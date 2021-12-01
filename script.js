66e03fc13add21027b9673407d1934889a412845
PhillyJB <philipbuuza@gmail.com>
2021-12-01 22:44:14 +0000

    added total earnings JS and average earning JS - both displayed in index.html

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-var dayOneInput = document.getElementById("text-area");
-dayOneInput.innerHTML = "ma";
 
+//capture the array of earning values and iterate through it
+var arrayOfDaysEarned = document.getElementsByClassName("x-day-temp");
+var totalEarnings = 0;
+for (eachDayWorked of arrayOfDaysEarned){
+   let amountEarnedOnDay = Number(eachDayWorked.innerHTML);
+   totalEarnings += amountEarnedOnDay;
+}
+
+//get the total earnings from iteration to insert in the HTML document to show user.
+const totalEarnedForTheMonth = document.getElementById("month-total");
+totalEarnedForTheMonth.innerHTML = totalEarnings;
+
+//average earned on a per day basis displayed to user
+const averageEarnedPerDayWorked = totalEarnings/arrayOfDaysEarned.length;
+console.log(averageEarnedPerDayWorked);
+const displayAverageEarnedPerDayWorked = document.getElementById("daily-average")
+displayAverageEarnedPerDayWorked.innerHTML = averageEarnedPerDayWorked;
